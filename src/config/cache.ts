8ff30fc2bc3adb9adf6b3dfecb0a4218e4ea8225
Turca Man <280507438+turcaman@users.noreply.github.com>
2026-09-17 01:02:20 -0600

    chore(cache): increase Home TTL to 12h and Details TTL to 24h

--- src/config/cache.ts
+++ src/config/cache.ts
@@ -7,8 +7,8 @@ export const CACHE_PREFIXES = {
 };
 
 export const CACHE_TTL = {
-  HOME: 6 * 60 * 60 * 1000,
-  DETAILS: 12 * 60 * 60 * 1000,
+  HOME: 12 * 60 * 60 * 1000,
+  DETAILS: 24 * 60 * 60 * 1000,
   SEARCH: 30 * 60 * 1000,
   SERVERS: 10 * 60 * 1000,
   STREAM: 5 * 60 * 1000,
